53d4f73584ed539af47277c36b7c4013b554305b
okyungjin <okyungjin@gmail.com>
2022-07-11 23:48:06 +0900

    REFACT change api name

--- src/api/CommonApi.ts
+++ src/api/CommonApi.ts
@@ -1,6 +1,6 @@
 import axios from '@/plugins/AxiosPlugin';
 
-export default class PostingApi {
+export default class CommonApi {
   public static checkHealth(): Promise<boolean> {
     return axios.get('/api/v1/health-check').then(res => res.data);
   }
