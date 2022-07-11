58338ac9a43d399418f2d5e570c219fff791ae31
okyungjin <okyungjin@gmail.com>
2022-07-11 23:31:32 +0900

    FEAT test mock api for health check

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,16 @@
-<template>Vue Infinity Scroll</template>
+<template>
+  <h1>Vue Infinity Scroll</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import CommonApi from '@/api/CommonApi';
+
+export default defineComponent({
+  async setup() {
+    const res = await CommonApi.checkHealth();
+    console.log(res);
+  },
+  emits: [],
+});
+</script>
