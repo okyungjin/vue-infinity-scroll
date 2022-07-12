eaed5901947537250ad6677177ebca1bb4582491
okyungjin <okyungjin@gmail.com>
2022-07-12 23:30:26 +0900

    FEAT add range utils

--- src/utils/CommonUtils.ts
+++ src/utils/CommonUtils.ts
@@ -2,4 +2,9 @@ export default class CommonUtils {
   public static generateRandomId() {
     return Math.random().toString(36).substring(2, 16);
   }
+
+  public static range(start: number, end: number) {
+    if (start > end) throw new RangeError();
+    return [...Array(end - start + 1).keys()].map(i => i + start);
+  }
 }
