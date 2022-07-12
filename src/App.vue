7c8dac31a38c2c15a6730f605f99a397e357b469
okyungjin <okyungjin@gmail.com>
2022-07-13 01:31:02 +0900

    FEAT set suspense loading for HomeView

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <Suspense>
-    <router-view />
+    <HomeView />
+    <template #fallback> Suspense Loading... </template>
   </Suspense>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import HomeView from '@/views/HomeView.vue';
 
-export default defineComponent({});
+export default defineComponent({
+  components: {
+    HomeView,
+  },
+});
 </script>
