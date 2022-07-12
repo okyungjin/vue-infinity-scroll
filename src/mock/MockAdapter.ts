962ba5d5d44c1e4601edada60691e4ca39d0c95d
okyungjin <okyungjin@gmail.com>
2022-07-13 01:30:08 +0900

    FEAT set delay to response for 1000ms

--- src/mock/MockAdapter.ts
+++ src/mock/MockAdapter.ts
@@ -1,4 +1,4 @@
 import MockAdapter from 'axios-mock-adapter';
 import axiosInstance from '@/plugins/AxiosPlugin';
 
-export default new MockAdapter(axiosInstance);
+export default new MockAdapter(axiosInstance, { delayResponse: 1000 });
